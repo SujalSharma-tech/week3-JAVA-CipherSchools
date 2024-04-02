4824ad58fc10256b194b5d590e115fd7b99912fe
SujalSharma-tech <112748674+SujalSharma-tech@users.noreply.github.com>
2024-04-02 17:31:21 +0530

    class35.java

---
+++ class35.java
@@ -0,0 +1,39 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] array = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        int target = 12;
+        int index = binarySearch(array, target);
+        if (index != -1) {
+            System.out.println("Element " + target + " found at index " + index);
+        } else {
+            System.out.println("Element " + target + " not found in the array");
+        }
+    }
+
+    // Binary search method without recursion
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Check if target is present at mid
+            if (array[mid] == target) {
+                return mid;
+            }
+
+            // If target is greater, ignore left half
+            if (array[mid] < target) {
+                left = mid + 1;
+            }
+            // If target is smaller, ignore right half
+            else {
+                right = mid - 1;
+            }
+        }
+
+        // Target not found in the array
+        return -1;
+    }
+}
